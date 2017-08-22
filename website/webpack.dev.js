28cf0d6b220da2549c1bc4cc4d17dcdf0be7766a
Norb7492 <norb7492@gmail.com>
2017-08-22 01:10:30 -0300

    webpack configs

---
+++ website/webpack.dev.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const optimize = require('webpack').optimize;
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+          
+module.exports = {
+
+    entry: {
+        bundle: path.resolve(__dirname, 'src') + '/app/index.js',
+        vendor: ['react', 'react-dom']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        filename: '[name].[hash].js',
+        sourceMapFilename: '[name].map'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+        devServer: {
+        inline: true,
+        contentBase: path.join(__dirname, 'src'),
+        hot: true,
+        port: 4200,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new optimize.UglifyJsPlugin(),
+        new optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            hash: true,
+            //favicon: 'src/app/assets/images/favicon.ico'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
+};
---
+++ website/webpack.prod.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const optimize = require('webpack').optimize;
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+          
+module.exports = {
+
+    entry: {
+        bundle: path.resolve(__dirname, 'src') + '/app/index.js',
+        vendor: ['react', 'react-dom']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        filename: '[name].[hash].js',
+        sourceMapFilename: '[name].map'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                use: ['file-loader?name=[name].[ext]&outputPath=app/assets/images/']
+            }
+        ]
+    },
+    plugins: [
+        new optimize.UglifyJsPlugin(),
+        new optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            hash: true
+        }),
+        new webpack.NamedModulesPlugin(),
+        new ExtractTextPlugin('app.css')
+    ]
+};
