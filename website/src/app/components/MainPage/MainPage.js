3597aac5f6312ad3c321430c010e0da94891b36b
Norb7492 <norb7492@gmail.com>
2017-08-22 14:29:34 -0300

    react bootstrap

--- website/src/app/components/MainPage/MainPage.js
+++ website/src/app/components/MainPage/MainPage.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './MainPage.scss';
-
+import { Button } from 'react-bootstrap';
 
 class MainPage extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello Nk Allianz</h1>
+        {/* <Button bsStyle="primary">Primary</Button> */}
       </div>
     );
   }
