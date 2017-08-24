7875090f9844ff66c88738ceca0cc562aaa04ee6
Kauê Nascimento <ksn7@ymail.com>
2017-08-24 11:28:42 -0300

    Tooltip on navbar language icons

--- website/src/app/components/NavBar/NavBar.js
+++ website/src/app/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './NavBar.scss';
 import { Link } from 'react-router-dom'
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem,Tooltip } from 'react-bootstrap';
 
 class NavBar extends Component {
   render() {
@@ -20,8 +20,15 @@ class NavBar extends Component {
         <NavItem eventKey={2}><Link to='/contact'><i className='fa fa-envelope'>&nbsp;</i>Contact</Link></NavItem>
       </Nav>
       <Nav pullRight>
-      <NavItem><img  className='Flags' src='app/assets/images/pt_BR.png'/></NavItem>
-      <NavItem><img  className='Flags' src='app/assets/images/en_US.png'/></NavItem>
+      <NavItem>
+      <a href='#' data-toggle='tooltip' title='Veja o site em portugues'>
+          <img alt='Veja o site em Português' className='Flags' src='app/assets/images/pt_BR.png'/>
+      </a>
+      </NavItem>
+      <NavItem>
+      <a href='#' data-toggle='tooltip' title='See that in English'>
+      <img className='Flags' src='app/assets/images/en_US.png'/></a>
+      </NavItem>
       <NavItem><img  className='Flags' src='app/assets/images/jp_JP.png'/></NavItem>
       <NavItem><img  className='Flags' src='app/assets/images/de_GE.png'/></NavItem>
       </Nav>
