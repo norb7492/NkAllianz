2b0bd741416f142bf9d2ac419e82e5816fe57fc9
Kauê Nascimento <ksn7@ymail.com>
2017-08-29 19:46:39 -0300

    Nao lembro o q eu fiz

--- website/src/app/components/NavBar/NavBar.js
+++ website/src/app/components/NavBar/NavBar.js
@@ -11,15 +11,16 @@ class NavBar extends Component {
   render() {
     return (
       <div className="NavBar">
-    <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
+    <Navbar className='navbar navbar-inverse navbar-static-top ' collapseOnSelect>
+    <Navbar.Header className='navbar-header'>
       <Navbar.Brand>
         <a href='/'><img className='Logo' title='NK Allianz' src="app/assets/images/nk-logo.png"/></a>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      <Nav pullLeft>
+   
+    <Nav pullLeft>
         <NavItem eventKey={1}><Link to='/'><i className='fa fa-home fa-lg'  aria-hidden='true'>&nbsp;</i>Home</Link></NavItem>
         <NavItem eventKey={2}><Link to='/contact'><i className='fa fa-envelope fa-fw' aria-hidden='true'>&nbsp;</i>Contact</Link></NavItem>
       </Nav>
@@ -29,6 +30,7 @@ class NavBar extends Component {
           <img alt='Veja o site em Português' className='Flags' src='app/assets/images/pt_BR.png'/>
       </a>
       </NavItem>
+
       <NavItem>
       <a href='#' data-toggle='tooltip' title='See that in English'>
       <img className='Flags' src='app/assets/images/en_US.png'/></a>
@@ -36,6 +38,7 @@ class NavBar extends Component {
       <NavItem><img  className='Flags' src='app/assets/images/jp_JP.png'/></NavItem>
       <NavItem><img  className='Flags' src='app/assets/images/de_GE.png'/></NavItem>
       </Nav>
+
     </Navbar.Collapse>
   </Navbar>     
       </div>
